b0a8b16cfdb4336a2a2d30e3aafa942df085c59a
Roman <asssfsa010@gmail.com>
2024-09-28 15:06:51 +0300

    Add project files.

---
+++ Olimpiada/TaskA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olimpiada
+{
+    class TaskA
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            int balance = 0; 
+            bool isDebt = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] action = Console.ReadLine().Split();
+                string type = action[0];
+                int amount = int.Parse(action[1]);
+
+                if (type == "earn")
+                {
+                    balance += amount; 
+                }
+                else if (type == "spend")
+                {
+                    balance -= amount; 
+                }
+
+                if (balance < 0)
+                {
+                    isDebt = true;
+                }
+            }
+
+            if (isDebt)
+            {
+                Console.WriteLine("debt");
+            }
+            else
+            {
+                Console.WriteLine("chinazes");
+            }
+        }
+    }
+}
---
+++ Olimpiada/TaskH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olimpiada
+{
+    class TaskH
+    {
+        static void Main()
+        {
+            string[] value = Console.ReadLine().Split(' ');
+            int a = int.Parse(value[0]);
+            int b = int.Parse(value[1]);
+            int c = int.Parse(value[2]);
+
+            int zagalnaSuma = (a >= b) ? a : a + c;
+
+            Console.WriteLine(zagalnaSuma);
+            Console.ReadKey();
+        }
+    }
+}
